5e5140a9c2a17ca27002ba35f84ac482b6befee8
ivan <nikitsenka@gmail.com>
2025-05-22 11:01:41 +0200

    connect chatbot to mcp server

--- chat.py
+++ chat.py
@@ -1,101 +1,120 @@
-import arxiv
-import json
-import os
-from typing import List
 from dotenv import load_dotenv
-import anthropic
+from anthropic import Anthropic
+from mcp import ClientSession, StdioServerParameters, types
+from mcp.client.stdio import stdio_client
+from typing import List
+import asyncio
+import nest_asyncio
 
-from extract_info import extract_info
-from search_papers import search_papers
+nest_asyncio.apply()
 
 load_dotenv()
-client = anthropic.Anthropic()
 
-mapping_tool_function = {
-    "search_papers": search_papers,
-    "extract_info": extract_info
-}
 
-with open("tools.json", "r") as json_file:
-    tools=json.load(json_file)
+class MCP_ChatBot:
 
-def execute_tool(tool_name, tool_args):
-    result = mapping_tool_function[tool_name](**tool_args)
+    def __init__(self):
+        # Initialize session and client objects
+        self.session: ClientSession = None
+        self.anthropic = Anthropic()
+        self.available_tools: List[dict] = []
 
-    if result is None:
-        result = "The operation completed but didn't return any results."
-
-    elif isinstance(result, list):
-        result = ', '.join(result)
-
-    elif isinstance(result, dict):
-        # Convert dictionaries to formatted JSON strings
-        result = json.dumps(result, indent=2)
-
-    else:
-        # For any other type, convert using str()
-        result = str(result)
-    return result
-
-def process_query(query):
-    messages = [{'role': 'user', 'content': query}]
-
-    response = client.messages.create(max_tokens=2024,
-                                      model='claude-3-7-sonnet-20250219',
-                                      tools=tools,
-                                      messages=messages)
-
-    process_query = True
-    while process_query:
-        assistant_content = []
-
-        for content in response.content:
-            if content.type == 'text':
-
-                print(content.text)
-                assistant_content.append(content)
-
-                if len(response.content) == 1:
-                    process_query = False
-
-            elif content.type == 'tool_use':
-
-                assistant_content.append(content)
-                messages.append({'role': 'assistant', 'content': assistant_content})
-
-                tool_id = content.id
-                tool_args = content.input
-                tool_name = content.name
-                print(f"Calling tool {tool_name} with args {tool_args}")
-
-                result = execute_tool(tool_name, tool_args)
-                messages.append({"role": "user",
-                                 "content": [
-                                     {
-                                         "type": "tool_result",
-                                         "tool_use_id": tool_id,
-                                         "content": result
-                                     }
-                                 ]
-                                 })
-                response = client.messages.create(max_tokens=2024,
+    async def process_query(self, query):
+        messages = [{'role': 'user', 'content': query}]
+        response = self.anthropic.messages.create(max_tokens=2024,
                                                   model='claude-3-7-sonnet-20250219',
-                                                  tools=tools,
+                                                  tools=self.available_tools,  # tools exposed to the LLM
                                                   messages=messages)
+        process_query = True
+        while process_query:
+            assistant_content = []
+            for content in response.content:
+                if content.type == 'text':
+                    print(content.text)
+                    assistant_content.append(content)
+                    if (len(response.content) == 1):
+                        process_query = False
+                elif content.type == 'tool_use':
+                    assistant_content.append(content)
+                    messages.append({'role': 'assistant', 'content': assistant_content})
+                    tool_id = content.id
+                    tool_args = content.input
+                    tool_name = content.name
 
-                if len(response.content) == 1 and response.content[0].type == "text":
-                    print(response.content[0].text)
-                    process_query = False
+                    print(f"Calling tool {tool_name} with args {tool_args}")
 
-if __name__ == '__main__':
-    print("Type your queries or 'exit' to exit.")
-    while True:
-        try:
-            query = input("\nQuery: ").strip()
-            if query.lower() == 'exit':
-                break
+                    # Call a tool
+                    # result = execute_tool(tool_name, tool_args): not anymore needed
+                    # tool invocation through the client session
+                    result = await self.session.call_tool(tool_name, arguments=tool_args)
+                    messages.append({"role": "user",
+                                     "content": [
+                                         {
+                                             "type": "tool_result",
+                                             "tool_use_id": tool_id,
+                                             "content": result.content
+                                         }
+                                     ]
+                                     })
+                    response = self.anthropic.messages.create(max_tokens=2024,
+                                                              model='claude-3-7-sonnet-20250219',
+                                                              tools=self.available_tools,
+                                                              messages=messages)
 
-            process_query(query)
-            print("\n")
-        except Exception as e:
-            print(f"\nError: {str(e)}")
+                    if (len(response.content) == 1 and response.content[0].type == "text"):
+                        print(response.content[0].text)
+                        process_query = False
+
+    async def chat_loop(self):
+        """Run an interactive chat loop"""
+        print("\nMCP Chatbot Started!")
+        print("Type your queries or 'quit' to exit.")
+
+        while True:
+            try:
+                query = input("\nQuery: ").strip()
+
+                if query.lower() == 'quit':
+                    break
+
+                await self.process_query(query)
+                print("\n")
+
+            except Exception as e:
+                print(f"\nError: {str(e)}")
+
+    async def connect_to_server_and_run(self):
+        # Create server parameters for stdio connection
+        server_params = StdioServerParameters(
+            command="python",  # Executable
+            args=["mcp_server.py"],  # Optional command line arguments
+            env=None,  # Optional environment variables
+        )
+        async with stdio_client(server_params) as (read, write):
+            async with ClientSession(read, write) as session:
+                self.session = session
+                # Initialize the connection
+                await session.initialize()
+
+                # List available tools
+                response = await session.list_tools()
+
+                tools = response.tools
+                print("\nConnected to server with tools:", [tool.name for tool in tools])
+
+                self.available_tools = [{
+                    "name": tool.name,
+                    "description": tool.description,
+                    "input_schema": tool.inputSchema
+                } for tool in response.tools]
+
+                await self.chat_loop()
+
+
+async def main():
+    chatbot = MCP_ChatBot()
+    await chatbot.connect_to_server_and_run()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
